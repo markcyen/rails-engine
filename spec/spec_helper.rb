8d044d7dc47d1d0e2fabac0bd1555470c44e29a2
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-16 21:10:59 -0600

    Set up rails and spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,14 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'factory_bot'
+require 'simplecov'
+SimpleCov.start do
+  add_filter "spec/rails_helper.rb"
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
