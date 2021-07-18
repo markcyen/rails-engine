b7d1eb5c9a12b5d8ef3221d29bc8db1b2697c828
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-18 11:10:14 -0600

    Create tests for default query and sad path limit over 20

--- spec/requests/api/v1/merchants_index_spec.rb
+++ spec/requests/api/v1/merchants_index_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  it 'sends a list of merchants' do
-    create_list(:merchant, 20)
-
+  before :each do
+    create_list(:merchant, 25)
+  end
+  it 'sends a list of 20 merchants for first page with default query params' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
@@ -20,5 +21,16 @@ describe 'Merchants API' do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
+  end
+
+  it 'sends a list of 20 merchants for first page with limit as query param' do
+    get '/api/v1/merchants', params: { data_limit: 35 }
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.size).to eq(20)
   end
 end
