f3b8df4eaa7b958503c6b8d4e66b11406dd537af
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-18 14:23:51 -0600

    Create test for sending limit merchants per page with sad paths

--- spec/requests/api/v1/merchants_index_spec.rb
+++ spec/requests/api/v1/merchants_index_spec.rb
@@ -1,49 +1,75 @@
 require 'rails_helper'
 
 describe 'Merchants API' do
-  before :each do
-    create_list(:merchant, 25)
-  end
-  it 'sends a list of twenty merchants for default query params' do
-    get '/api/v1/merchants'
+  describe 'default' do
+    it 'sends a list of twenty merchants for default query params' do
+      create_list(:merchant, 41)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      get '/api/v1/merchants'
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    expect(merchants.size).to eq(20)
+      merchants = JSON.parse(response.body, symbolize_names: true)
 
-    merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchants.size).to eq(20)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(Integer)
+
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
     end
   end
 
-  it 'sends a list of twenty merchants for default page one with limit as query param' do
-    get '/api/v1/merchants', params: { data_limit: 35 }
+  describe 'default page, limit query greater than 20' do
+    it 'sends a list of twenty merchants for default page one with limit as query param' do
+      create_list(:merchant, 41)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      get '/api/v1/merchants', params: { data_limit: 35 }
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    expect(merchants.size).to eq(20)
-    expect(merchants.last[:id]).to eq(20)
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.size).to eq(20)
+      expect(merchants.last[:id]).to eq(20)
+    end
   end
 
-  it 'sends a list of twenty merchants for default limit of twenty with page zero' do
-    get '/api/v1/merchants', params: { page: 0 }
+  describe 'default limit, page zero' do
+    it 'sends a list of twenty merchants for default limit of twenty with page zero' do
+      create_list(:merchant, 41)
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      get '/api/v1/merchants', params: { page: 0 }
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    expect(merchants.size).to eq(20)
-    expect(merchants.last[:id]).to eq(20)
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.size).to eq(20)
+      expect(merchants.last[:id]).to eq(20)
+    end
   end
+
+  describe 'default limit, page two' do
+    it 'sends a list of twenty merchants for default limit of twenty with page two' do
+      create_list(:merchant, 41)
+
+      get '/api/v1/merchants', params: { page: 2 }
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.size).to eq(20)
+      expect(merchants.last[:id]).to eq(40)
+    end
+  end
+
 end
