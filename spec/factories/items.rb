64348177e197c2749cf8cd2ea72ea676152db982
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-18 10:01:32 -0600

    Add association

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Quote.yoda }
     unit_price { Faker::Number.number(digits: 7) }
-    merchant { nil }
+    association :merchant, factory: :merchant
   end
 end
