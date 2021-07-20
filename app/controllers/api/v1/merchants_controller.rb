3f5aa3184c6c5dbca9888eef2307d3af6e054d5d
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-20 14:36:13 -0600

    Modify merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,37 +3,78 @@ class Api::V1::MerchantsController < ApplicationController
   PAGE_DEFAULT = 1
 
   def index
-    # page = params.fetch(:page, 0).to_i
-# binding.pry
     if !params[:data_limit].present? && !params[:page].present?
-      render json: Merchant.limit_merchants_per_page
+      render json: Merchant.limit(20)
     elsif params[:data_limit].present? && !params[:page].present?
       if params[:data_limit].to_i < 0
-        render json: Merchant.limit_merchants_per_page
+        render json: {status: 400, message: "Negative query results to error."}
       else
-        render json: Merchant.limit_merchants_per_page(params[:data_limit], PAGE_DEFAULT)
+        render json: Merchant.limit(params[:data_limit].to_i)
       end
     elsif !params[:data_limit].present? && params[:page].present?
-      if params[:page].to_i <= 0
-        render json: Merchant.limit_merchants_per_page
+      if params[:page].to_i < 0
+        render json: {status: 400, message: "Negative or zero query results to error."}
+      elsif params[:page].to_i == 0
+        render json: Merchant.limit(20)
       else
-        render json: Merchant.limit_merchants_per_page(DATA_LIMIT, params[:page])
+        render json: Merchant.limit(20).offset((params[:page].to_i - 1) * 20)
       end
     else
-      if params[:data_limit].to_i < 0
-        render json: Merchant.limit_merchants_per_page(DATA_LIMIT, params[:page])
-      elsif params[:data_limit].to_i < DATA_LIMIT
-        render json: Merchant.limit_merchants_per_page(params[:data_limit].to_i % DATA_LIMIT)
-      elsif params[:page].to_i <= 0
-        render json: Merchant.limit_merchants_per_page(params[:data_limit])
-      else
-        render json: Merchant.limit_merchants_per_page(params[:data_limit], params[:page])
+      if params[:data_limit].present? && params[:page].present?
+        if (params[:data_limit].to_i < 0 && params[:page].to_i < 1) ||
+          (params[:data_limit].to_i < 0 && params[:page].to_i > 1) ||
+          (params[:data_limit].to_i > 0 && params[:page].to_i < 1)
+            render json: {status: 400, message: "Negative query results to error."}
+        else
+          render json: Merchant.limit(params[:data_limit].to_i).offset((params[:page].to_i - 1) * params[:data_limit].to_i)
+        end
       end
     end
+
+#     if !params[:page].present?
+#       page = 1
+#     elsif params[:page].present?
+#       if params[:page].to_i < 1
+#         page = 1
+#       else
+#         page = params[:page].to_i
+#       end
+#     end
+# # binding.pry
+
+#
+#     if params[:data_limit].present? && !params[:page].present?
+#       if params[:data_limit].to_i < 0
+#         render json: {status: 400, message: "Negative query results to error."}
+#       elsif params[:data_limit].to_i > 20
+#         render json: Merchant.limit(20)
+#       else
+#         render json: Merchant.limit(params[:data_limit].to_i)
+#       end
+#     end
+#
+#     if !params[:data_limit].present? && params[:page].present?
+#       # if params[:page].to_i < 1
+#       #   render json: {status: 400, message: "Negative or zero query results to error."}
+#       # else
+#         render json: Merchant.limit(20).offset((page - 1) * 20)
+#       # end
+#     end
+#
+#     if params[:data_limit].present? && params[:page].present?
+#       if (params[:data_limit].to_i < 0 && params[:page].to_i < 1) ||
+#         (params[:data_limit].to_i < 0 && params[:page].to_i > 1) ||
+#         (params[:data_limit].to_i > 0 && params[:page].to_i < 1)
+#           render json: {status: 400, message: "Negative query results to error."}
+#       else
+#         render json: Merchant.limit(params[:data_limit].to_i).offset((page - 1) * params[:data_limit].to_i)
+#       end
+#     end
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 
 end
