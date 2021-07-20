a539d1212743d27c52e85cae582bc6f5b6d57d8c
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-20 15:09:38 -0600

    Refactor controller to pass tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,75 +1,40 @@
 class Api::V1::MerchantsController < ApplicationController
-  DATA_LIMIT = 20
-  PAGE_DEFAULT = 1
+  # DATA_LIMIT = 20
+  # PAGE_DEFAULT = 1
 
   def index
-    if !params[:data_limit].present? && !params[:page].present?
-      render json: Merchant.limit(20)
-    elsif params[:data_limit].present? && !params[:page].present?
-      if params[:data_limit].to_i < 0
+    if !params[:per_page].present? && !params[:page].present?
+      merchants = Merchant.limit(20)
+      render json: MerchantSerializer.new(merchants)
+    elsif params[:per_page].present? && !params[:page].present?
+      if params[:per_page].to_i < 0
         render json: {status: 400, message: "Negative query results to error."}
       else
-        render json: Merchant.limit(params[:data_limit].to_i)
+        merchants = Merchant.limit(params[:per_page].to_i)
+        render json: MerchantSerializer.new(merchants)
       end
-    elsif !params[:data_limit].present? && params[:page].present?
+    elsif !params[:per_page].present? && params[:page].present?
       if params[:page].to_i < 0
         render json: {status: 400, message: "Negative or zero query results to error."}
       elsif params[:page].to_i == 0
-        render json: Merchant.limit(20)
+        merchants = Merchant.limit(20)
+        render json: MerchantSerializer.new(merchants)
       else
-        render json: Merchant.limit(20).offset((params[:page].to_i - 1) * 20)
+        merchants = Merchant.limit(20).offset((params[:page].to_i - 1) * 20)
+        render json: MerchantSerializer.new(merchants)
       end
     else
-      if params[:data_limit].present? && params[:page].present?
-        if (params[:data_limit].to_i < 0 && params[:page].to_i < 1) ||
-          (params[:data_limit].to_i < 0 && params[:page].to_i > 1) ||
-          (params[:data_limit].to_i > 0 && params[:page].to_i < 1)
+      if params[:per_page].present? && params[:page].present?
+        if (params[:per_page].to_i < 0 && params[:page].to_i < 1) ||
+          (params[:per_page].to_i < 0 && params[:page].to_i > 1) ||
+          (params[:per_page].to_i > 0 && params[:page].to_i < 1)
             render json: {status: 400, message: "Negative query results to error."}
         else
-          render json: Merchant.limit(params[:data_limit].to_i).offset((params[:page].to_i - 1) * params[:data_limit].to_i)
+          merchants = Merchant.limit(params[:per_page].to_i).offset((params[:page].to_i - 1) * params[:per_page].to_i)
+          render json: MerchantSerializer.new(merchants)
         end
       end
     end
-
-#     if !params[:page].present?
-#       page = 1
-#     elsif params[:page].present?
-#       if params[:page].to_i < 1
-#         page = 1
-#       else
-#         page = params[:page].to_i
-#       end
-#     end
-# # binding.pry
-
-#
-#     if params[:data_limit].present? && !params[:page].present?
-#       if params[:data_limit].to_i < 0
-#         render json: {status: 400, message: "Negative query results to error."}
-#       elsif params[:data_limit].to_i > 20
-#         render json: Merchant.limit(20)
-#       else
-#         render json: Merchant.limit(params[:data_limit].to_i)
-#       end
-#     end
-#
-#     if !params[:data_limit].present? && params[:page].present?
-#       # if params[:page].to_i < 1
-#       #   render json: {status: 400, message: "Negative or zero query results to error."}
-#       # else
-#         render json: Merchant.limit(20).offset((page - 1) * 20)
-#       # end
-#     end
-#
-#     if params[:data_limit].present? && params[:page].present?
-#       if (params[:data_limit].to_i < 0 && params[:page].to_i < 1) ||
-#         (params[:data_limit].to_i < 0 && params[:page].to_i > 1) ||
-#         (params[:data_limit].to_i > 0 && params[:page].to_i < 1)
-#           render json: {status: 400, message: "Negative query results to error."}
-#       else
-#         render json: Merchant.limit(params[:data_limit].to_i).offset((page - 1) * params[:data_limit].to_i)
-#       end
-#     end
   end
 
   def show
