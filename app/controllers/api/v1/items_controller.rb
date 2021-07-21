b4d8c0a16deb3b06ef9491222fada738ac0446b2
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-20 18:27:28 -0600

    Implement index action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,40 @@
 class Api::V1::ItemsController < ApplicationController
 
+  def index
+    if !params[:per_page].present? && !params[:page].present?
+      items = Item.limit(20)
+      render json: ItemSerializer.new(items)
+    elsif params[:per_page].present? && !params[:page].present?
+      if params[:per_page].to_i < 0
+        render json: {status: 400, message: "Negative query results to error."}
+      else
+        items = Item.limit(params[:per_page].to_i)
+        render json: ItemSerializer.new(items)
+      end
+    elsif !params[:per_page].present? && params[:page].present?
+      if params[:page].to_i < 0
+        render json: {status: 400, message: "Negative or zero query results to error."}
+      elsif params[:page].to_i == 0
+        items = Item.limit(20)
+        render json: ItemSerializer.new(items)
+      else
+        items = Item.limit(20).offset((params[:page].to_i - 1) * 20)
+        render json: ItemSerializer.new(items)
+      end
+    else
+      if params[:per_page].present? && params[:page].present?
+        if (params[:per_page].to_i < 0 && params[:page].to_i < 1) ||
+          (params[:per_page].to_i < 0 && params[:page].to_i > 1) ||
+          (params[:per_page].to_i > 0 && params[:page].to_i < 1)
+            render json: {status: 400, message: "Negative query results to error."}
+        else
+          items = Item.limit(params[:per_page].to_i).offset((params[:page].to_i - 1) * params[:per_page].to_i)
+          render json: ItemSerializer.new(items)
+        end
+      end
+    end
+  end
+
   def show
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item)
