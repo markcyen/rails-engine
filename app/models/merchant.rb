426519e645f4830c7bc0ad66317f822c17ce4ab1
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-21 12:52:04 -0600

    Implelment revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+
+  def revenue
+    invoices.joins(:invoice_items, :transactions)
+      # .group(':id', 'invoices.id')
+      .where('transactions.result = ? AND invoices.status = ?', "success", "shipped")
+      .sum('invoice_items.quantity * invoice_items.unit_price')
+      .round(2)
+  end
 end
