dddc7264685667b53977b3fe57536d984b43ee18
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-21 17:04:26 -0600

    Implement index action

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
+  def index
+    # binding.pry
+    if !params[:quantity].present? || params[:quantity].to_i < 1
+      render json: {status: 400, message: "Need a relevant quantity input."}
+    else
+      render json: MostRevenueSerializer.new(Merchant.top_revenue(params[:quantity]))
+    end
+  end
+
   def show
     merchant = Merchant.find(params[:id])
-    # binding.pry
-    # merchant_revenue = merchant.revenue.round(2)
     render json: MerchantRevenueSerializer.new(merchant)
   end
 end
