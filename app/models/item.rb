b318b02a4c6ea7747b0e480ee2a6fa6b39324dc3
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-20 19:55:21 -0600

    Implement validations and relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
-  validates :name, :description, :unit_price, presence: true
+  validates_presence_of :name, :description, :unit_price
   validates :unit_price, numericality: true
-  
+
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
   belongs_to :merchant
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
   has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
+  has_many :transactions, through: :invoices
 end
