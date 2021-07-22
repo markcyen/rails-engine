79224488b8224ba858c09e95b7ad0ec3bb296151
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-22 17:33:36 -0600

    Create test for item search method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe Item, type: :model do
     it {should have_many(:invoices).through(:invoice_items)}
     it {should have_many(:transactions).through(:invoices)}
   end
+
+  describe '::search' do
+    it 'can search for merchant by name' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, name: 'Ice cream', merchant: merchant_1)
+      item_2 = create(:item, name: 'Chocolate cake', merchant: merchant_1)
+      item_3 = create(:item, name: 'Cream cheese', merchant: merchant_1)
+      item_4 = create(:item, name: 'Strawberry cream cheese', merchant: merchant_1)
+
+      expect(Item.search('cream').first).to eq(item_1)
+      expect(Item.search('cream').second).to eq(item_4)
+      expect(Item.search('Cream').first).to eq(item_3)
+      expect(Item.search('stew')).to eq([])
+    end
+  end
 end
