8778f3f4bd63d5c6584a673353fc9e2015c90d17
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-23 00:04:58 -0600

    Modify conditional

--- app/controllers/api/v1/revenue/unshipped_controller.rb
+++ app/controllers/api/v1/revenue/unshipped_controller.rb
@@ -1,19 +1,17 @@
 class Api::V1::Revenue::UnshippedController < ApplicationController
   CONSTANT = 10
+  
   def index
-    # binding.pry
     if params[:quantity].to_i < 0 ||
       params[:quantity] == "" ||
-      !params[:quantity].to_s.scan(/\D/).empty?
-        render json: {status: 400, message: "Need a relevant quantity input."}
+      !params[:quantity].to_s.scan(/\D/).empty? ||
+      Invoice.unshipped_potential(params[:quantity]).nil?
+        render json: {data: {}, status: 400, message: "Need a relevant quantity input."}
     elsif !params[:quantity].present? ||
       params[:quantity].nil? ||
       params[:quantity].empty? ||
       params[:quantity].to_i == 0
-        invoices = Invoice.unshipped_potential(CONSTANT) # .map do |invoice|
-          # binding.pry
-          # Hash.new { |hash, key| hash[invoice.id] = key[invoice.potential_revenue] }
-        # end
+        invoices = Invoice.unshipped_potential(CONSTANT)
         render json: UnshippedSerializer.new(invoices)
     else
       invoices = Invoice.unshipped_potential(params[:quantity].to_i)
